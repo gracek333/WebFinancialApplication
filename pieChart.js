82834759dcb244531e17b6e608ad6b267ca894b1
Michal Gracon <michalgracon@onet.pl>
2021-08-03 01:56:16 +0200

    Added pie chart showing

---
+++ pieChart.js
@@ -0,0 +1,39 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Kategoria przychodów', 'Przychody [zł]'],
+  ['Jedzenie', 1000.35],
+  ['Mieszkanie', 1500.65],
+  ['Transport', 102.48],
+  ['Telekomunikacja', 100.59],
+  ['Opieka zdrowotna', 0],
+  ['Ubranie', 0],
+  ['Higiena', 50],
+  ['Dzieci ', 200],
+  ['Rozrywka', 100],
+  ['Wycieczka', 0],
+  ['Szkolenia', 0],
+  ['Książki', 100],
+  ['Oszczędności', 200],
+  ['Na emeryturę', 0],
+  ['Spłata długów', 0],
+  ['Darowizna', 0],
+  ['Inne', 0]
+]);
+
+  var options = {'width': 'auto', 'height': 400};
+
+	/*if (window.innerWidth >= 730) {
+	  var options = {'width':650, 'height':400};
+	} else if (window.innerWidth <= 730 && window.innerWidth >= 535) {
+	var options = {'width':450, 'height':400};}
+	 else
+	{
+	  var options = {'width':300, 'height':400};
+	}*/
+
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
